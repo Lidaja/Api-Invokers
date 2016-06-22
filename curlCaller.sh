dbdf03c01de8316b699b9c8197179af67d79cd3c
Liam Jackson <lijax@hotmail.com>
2016-06-22 23:43:05 +0200

    ad

--- curlCaller.sh
+++ curlCaller.sh
@@ -1,7 +1,7 @@
-curlId=$(curl --unix-socket /var/run/system-docker.sock -s -H "Content-Type: application/json" -X POST -d  '{"AttachStdin": false, "AttachStdout": true, "AttachStderr": true, "Tty": false, "Cmd": ["python", "/usr/lib/python2.7/Api-Invokers/invokers/createCluster.py", "10", "10" ] }' http:/containers/console/exec | jq '.Id' | tr -d '\"')
-echo $curlId
+curlId=$(curl --unix-socket /var/run/system-docker.sock -s -H "Content-Type: application/json" -X POST -d  '{"AttachStdin": false, "AttachStdout": true, "AttachStderr": true, "Tty": false, "Cmd": ["python", "/usr/lib/python2.7/Api-Invokers/invokers/createZFSPool.py", "10", "10" ] }' http:/containers/console/exec | jq '.Id' | tr -d '\"')
+#echo $curlId
 command="curl --unix-socket /var/run/system-docker.sock -s -H \"Content-Type: application/json\" -X POST -d '{\"Detach\": false, \"Tty\": true }' http:/exec/$curlId/start"
-echo $command
+#echo $command
 result=$(eval $command)
 echo $result
 
