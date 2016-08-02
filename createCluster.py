c4b2c63da420eb8ac2140a6a94144c9380c4c39a
Liam Jackson <liam.jackson@istuary.com>
2016-08-02 09:55:45 -0400

    fixed syntax

--- createCluster.py
+++ createCluster.py
@@ -1,14 +1,19 @@
 import sys
 sys.path.append('/usr/lib/python2.7')
-from denali.cluster import Cluster
+#from denali.cluster import Cluster
 
 
-def createCluster(node_ip,vip):
+def createCluster(node_ip,vip,numNodes):
+	nodes = []
+	last = node_ip.split(".")[-1]
+	first = ".".join(node_ip.split(".")[:-1])+"."
+	for i in range(0,int(numNodes)):
+		nodes.append(first+str(int(last)+i))
 	cluster = Cluster('denaliCluster')
 	try:
 		cluster.restore()
 	except ValueError as e:
-		cluster.add_node(node_ip)
+		cluster.nodes = nodes
 		cluster.vip = vip
 		cluster.nic = 'eth0'
 		cluster.netmask = '24'
@@ -16,4 +21,4 @@ def createCluster(node_ip,vip):
 	return cluster
 
 if __name__=='__main__':
-	createCluster(sys.argv[1],sys.argv[2])
+	createCluster(sys.argv[1],sys.argv[2],sys.argv[3])
